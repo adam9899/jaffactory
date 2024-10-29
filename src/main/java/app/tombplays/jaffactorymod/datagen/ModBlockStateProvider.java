29ad30656edf85e7388dc73b50351b64a6f092f9
TombPlays <dave.tattersall@hotmail.co.uk>
2024-10-29 14:28:36 +0000

    Added orange.png

--- src/main/java/app/tombplays/jaffactorymod/datagen/ModBlockStateProvider.java
+++ src/main/java/app/tombplays/jaffactorymod/datagen/ModBlockStateProvider.java
@@ -3,15 +3,22 @@ package app.tombplays.jaffactorymod.datagen;
 import app.tombplays.jaffactorymod.JaffactoryMod;
 import app.tombplays.jaffactorymod.block.ModBlocks;
 import net.minecraft.data.PackOutput;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.client.model.generators.ModelFile;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.registries.RegistryObject;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class ModBlockStateProvider extends BlockStateProvider {
+    private final PackOutput packOutput;
+
     public ModBlockStateProvider(PackOutput output, ExistingFileHelper exFileHelper) {
         super(output, JaffactoryMod.MODID, exFileHelper);
+        this.packOutput = output;
     }
 
     @Override
@@ -20,7 +27,11 @@ public class ModBlockStateProvider extends BlockStateProvider {
     }
 
     private void blockWithItem(RegistryObject<Block> blockRegistryObject) {
-        simpleBlockWithItem(blockRegistryObject.get(), cubeAll(blockRegistryObject.get()));
+        String texturePath = "block/" + blockRegistryObject.getId().getPath();
+        ResourceLocation textureLocation = modLoc(texturePath);
+
+        // Log the texture path
+        System.out.println("Using texture path: " + texturePath);
     }
 
 }
